9eddca97c7ad7ea72e7b3b1f190c74aac7cabeb7
emepica <emeline.picard@hotmail.com>
2021-11-21 15:31:26 +0100

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
+    <div className="container">
     <div className="App">
       <header className="App-header">
-       Hello:)
+       Dictionary
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer>This project was built by Emeline Picard and is <a href="https://github.com/emepica/dictionary-react">open-sourced</a></footer>
+    </div>
     </div>
   );
 }
