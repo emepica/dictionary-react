ec761adefd29b09329abed824231518dc50e5cdb
emepica <emeline.picard@hotmail.com>
2021-11-24 09:31:03 +0100

    Boolean state to get a default word

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Dictionary from './Dictionary';
 
 function App() {
   return (
-    <div className="container">
+    
     <div className="App">
+      <div className="container">
       <header className="App-header">
        Dictionary
       </header>
       <main>
-        <Dictionary/>
+        <Dictionary defaultKeyword="wave"/>
       </main>
       <footer>This project was built by Emeline Picard and is <a href="https://github.com/emepica/dictionary-react">open-sourced</a></footer>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,32 +4,49 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("Wave");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results,setResults]= useState(null);
+  let [loaded,setLoaded]=useState(false);
 
   function handleResponse(response){
     setResults(response.data[0]);
   }
   
-  //API doc : https://dictionaryapi.dev/
-  function search(event) {
-    event.preventDefault();
+  
+  function search(){
+    //API doc : https://dictionaryapi.dev/
     let apiURL =`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiURL).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
+  function load(){
+    setLoaded(true);
+    search();
+  }
+ if (loaded){
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <section>
+      <form onSubmit={handleSubmit}>
         <input type="search" className="Search-Bar" placeholder="Type a word" onChange={handleKeywordChange} />
         <button type="Sumbit">Search</button>
       </form>
+      <div className="hint">
+        Suggested words: sunset, wine, dance, plants, ... 
+      </div>
+      </section>
       <Results results={results}/>
     </div>
-  );
+  );} else { 
+    load();
+    return "Loading";}
 }
